10ad3ec8b97100790f96a2937e6cf0168ffa7236
yogadarma13 <kadekyoga125@gmail.com>
2023-08-31 19:31:05 +0800

    Create new activity for main list page

---
+++ main-list/src/main/java/com/yogadarma/main_list/MainListActivity.kt
@@ -0,0 +1,10 @@
+package com.yogadarma.main_list
+
+import com.yogadarma.common.base.BaseActivity
+import com.yogadarma.main_list.databinding.ActivityMainListBinding
+
+class MainListActivity : BaseActivity<ActivityMainListBinding>(ActivityMainListBinding::inflate) {
+    override fun onView() {
+
+    }
+}
