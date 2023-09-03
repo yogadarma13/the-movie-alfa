6835f13828e7cc3737a8c44aab28e81626619ffe
yogadarma13 <kadekyoga125@gmail.com>
2023-09-04 02:01:22 +0800

    Create function to insert video and get video data on VideoDao

--- core/src/main/java/com/yogadarma/core/data/source/local/room/dao/VideoDao.kt
+++ core/src/main/java/com/yogadarma/core/data/source/local/room/dao/VideoDao.kt
@@ -1,8 +1,17 @@
 package com.yogadarma.core.data.source.local.room.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.yogadarma.core.data.source.local.room.entity.VideoEntity
 
 @Dao
 interface VideoDao {
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertVideo(videoEntity: VideoEntity)
+
+    @Query("SELECT * FROM video WHERE id=:id")
+    suspend fun getVideo(id: String): VideoEntity
 }
