96fd62226c0a24ed064a0b8ec29144a9fc3d1598
yogadarma13 <kadekyoga125@gmail.com>
2023-09-04 05:00:16 +0800

    Move to Main List Page from Splash Screen Page

--- app/src/main/java/com/yogadarma/themoviealfa/navigation/ActivityMapper.kt
+++ app/src/main/java/com/yogadarma/themoviealfa/navigation/ActivityMapper.kt
@@ -1,8 +1,10 @@
 package com.yogadarma.themoviealfa.navigation
 
 import com.yogadarma.common.navigation.NavigationDirection
+import com.yogadarma.main_list.MainListActivity
 import com.yogadarma.movie_detail.MovieDetailActivity
 
 val activityMapper = mapOf(
+    NavigationDirection.MainList::class.java to MainListActivity::class.java,
     NavigationDirection.MovieDetail::class.java to MovieDetailActivity::class.java
 )
--- common/src/main/java/com/yogadarma/common/navigation/NavigationDirection.kt
+++ common/src/main/java/com/yogadarma/common/navigation/NavigationDirection.kt
@@ -3,5 +3,6 @@ package com.yogadarma.common.navigation
 import com.yogadarma.common.BuildConfig
 
 sealed class NavigationDirection(val extras: Map<String, Any?>) {
-    class MovieDetail(movieId: String): NavigationDirection(mapOf(BuildConfig.MOVIE_ID to movieId))
+    object MainList : NavigationDirection(mapOf())
+    class MovieDetail(movieId: String) : NavigationDirection(mapOf(BuildConfig.MOVIE_ID to movieId))
 }
